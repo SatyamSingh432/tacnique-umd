1524f0e8b3043aa0dfebbc83935e306558684051
Satyam Singh <satyamrajput7239@gmail.com>
2024-11-27 00:40:17 +0530

    feat: Implement Edit User

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -4,7 +4,9 @@ import UserForm from "./UserForm";
 
 const TableRow = ({ user }) => {
   const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+  const [editUser, setEditUser] = useState(user);
   const { id, name, email, address } = user;
+
   return (
     <>
       <tr className="row">
@@ -26,6 +28,16 @@ const TableRow = ({ user }) => {
         isFormOpen={isEditFormOpen}
         setIsFormOpen={setIsEditFormOpen}
         isEdit
+        user={{
+          name: name,
+          email: email,
+          street: address?.street,
+          suite: address?.suite,
+          city: address?.city,
+          phone: editUser?.phone,
+          companyName: editUser?.company?.name,
+        }}
+        setUser={setEditUser}
       />
     </>
   );
